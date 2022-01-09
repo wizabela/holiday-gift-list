79744cfdb838a61705fccfeb3c1dc6089116385f
id github: wizabela <izabela.wyrebska@gmail.com>
2022-01-09 16:27:56 +0100

    add mpost method for adding gifts and numbers of gifts to list

--- records/gift.record.js
+++ records/gift.record.js
@@ -1,17 +1,35 @@
+const {pool} = require("../utils/db");
+const {ValidationError} = require("../utils/errors");
+const {v4: uuid} = require('uuid');
+
 class GiftRecord {
-    static listAll() {
-        return [
-            {
-                id: 'abc1',
-                name: 'Tamburyn',
-                count: 5,
-            },
-            {
-                id: 'abc2',
-                name: 'Koparka',
-                count: 3,
-            },
-        ];
+    constructor(obj) {
+        if (obj.name || obj.name.length < 3 || obj.name.length > 55) {
+            throw new ValidationError('Nazwa prezentu musi mieć od 3 do 55 znaków.');
+        }
+        if (obj.count || obj.count < 1 || obj.count > 999999) {
+            throw new ValidationError('Liczba szt. prezentów powinna się mieścić w przedziale 1 - 999999.');
+        }
+        this.id = obj.id;
+        this.name = obj.name;
+        this.count = obj.count;
+    }
+
+    async insert() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+        pool.execute("INSERT INTO `gifts` VALUES(:id, :name, :count)", {
+            id: this.id,
+            name: this.name,
+            count: this.count,
+        });
+        return this.id;
+    }
+
+    static async listAll() {
+        const [result] = await pool.execute("SELECT * FROM `gifts`");
+        return result;
     }
 }
 
--- routers/gift.js
+++ routers/gift.js
@@ -5,12 +5,22 @@ const giftRouter = Router();
 
 giftRouter
 
-    .get('/', (req, res) => {
-        const giftsList = GiftRecord.listAll();
+    .get('/', async (req, res) => {
+        const giftsList = await GiftRecord.listAll();
 
         res.render('gift/list', {
             giftsList,
         });
+    })
+    .post('/', async (req, res) => {
+        const data = {
+            ...req.body,
+            count: Number(req.body.count),
+        };
+        console.log(data);
+        // res.render('gift/list', {
+        //     giftsList,
+        // });
     });
 
 module.exports = {
