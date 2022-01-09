f6fd76eea8cdfbce88f059b15a23cd75a91c78d3
id github: wizabela <izabela.wyrebska@gmail.com>
2022-01-09 12:34:26 +0100

    make view for selecting gift from dropdown list

--- routers/child.js
+++ routers/child.js
@@ -1,17 +1,18 @@
 const {Router} = require("express");
 const {ChildRecord} = require("../records/child.record");
+const {GiftRecord} = require("../records/gift.record");
 
 const childRouter = Router();
-
-
 
 childRouter // wszystkie ścieżki zaczynają się od '/child'
 
     .get('/', (req, res) => {
         const childrenList = ChildRecord.listAll();
+        const giftsList = GiftRecord.listAll();
 
         res.render('children/list', {
             childrenList,
+            giftsList,
         });
     });
 
